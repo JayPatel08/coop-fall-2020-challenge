46675572eabf7c81b2bf874205845b41fccbc6af
JayPatel08 <46641819+JayPatel08@users.noreply.github.com>
2020-07-10 14:18:32 -0400

    final commit with passed test cases

--- solution_python.py
+++ solution_python.py
@@ -1,23 +1,51 @@
 class EventSourcer():
-    # Do not change the signature of any functions
-
+    # Do not change the operatorature of any functions
     def __init__(self):
         self.value = 0
+        # list of tuples(<num,operation>) to keep track of undo actions
+        self.undoActions = []
+        # list of tuples(<num,operation>) to keep track of redo actions
+        self.redoActions = []
 
     def add(self, num: int):
-        pass
+        self.value += num
+        self.undoActions.append((num, '+'))
 
     def subtract(self, num: int):
-        pass
+        self.value -= num
+        self.undoActions.append((num, '-'))
 
     def undo(self):
-        pass
+        if len(self.undoActions) == 0:
+            return
+        num, operator = self.undoActions[-1]
+        del self.undoActions[-1]
+        if operator == '+':
+            self.value -= num
+        else:
+            self.value += num
+        self.redoActions.append((num, operator))
 
     def redo(self):
-        pass
+        if len(self.redoActions) == 0:
+            return
+        num, operator = self.redoActions[-1]
+        del self.redoActions[-1]
+        if operator == '+':
+            self.value += num
+        else:
+            self.value -= num
+
+        self.undoActions.append((num, operator))
 
     def bulk_undo(self, steps: int):
-        pass
+        if steps > len(self.undoActions):
+            steps = len(self.undoActions)
+        for i in range(steps):
+            self.undo()
 
     def bulk_redo(self, steps: int):
-        pass
+        if steps > len(self.redoActions):
+            steps = len(self.redoActions)
+        for i in range(steps):
+            self.redo() 
